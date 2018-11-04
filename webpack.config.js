129610741014b076e1a4c669b546b99ad7aa6d79
Dmitriy Shin <dimashin9@gmail.com>
2018-11-04 17:40:06 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const Webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const OptimizeCSSAssets = require("optimize-css-assets-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = env => {
+  return {
+    // Entry
+    entry: "./src/index.js",
+
+    // Output
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "bundle.[hash].js"
+    },
+
+    // Module
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: "babel-loader",
+          exclude: /node_modules/
+        },
+
+        {
+          test: /\.(css|scss|sass)$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          exclude: /node_modules/
+        },
+
+        {
+          test: /\.(png|svg|gif|jpg|jpeg)$/,
+          loader: "file-loader",
+          excude: /node_modules/
+        }
+      ]
+    },
+
+    // Plugins
+    plugins: [
+      new Webpack.ProgressPlugin(),
+      new HTMLWebpackPlugin({ template: "./src/index.html" })
+    ],
+
+    // Developer Tool
+    devtool: env.mode === "production" ? "source-map" : "cheap-eval-source-map",
+
+    // Development Server Configuration
+    devServer: {},
+
+    // Optimization
+    optimization: {
+      minimizer: [
+        new UglifyJSPlugin({
+          uglifyOptions: {
+            compress: {
+              unsafe: true,
+              inline: true,
+              passes: 2,
+              keep_fargs: false
+            },
+            output: {
+              beautify: false
+            },
+            mangle: true
+          }
+        }),
+
+        new OptimizeCSSAssets({
+          cssProcessorOptions: {
+            preset: "advanced",
+            safe: true,
+            map: { inline: false }
+          }
+        })
+      ]
+    }
+  };
+};
